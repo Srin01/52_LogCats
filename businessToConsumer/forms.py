536da59afe8969b08d5e6e3ee88021d6b9fef13c
Srinidhi S Prasad <64426998+Srin01@users.noreply.github.com>
2021-07-11 09:21:11 +0530

    Added Item in models

--- businessToConsumer/forms.py
+++ businessToConsumer/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django_countries.widgets import CountrySelectWidget
 from django_countries.fields import CountryField
 from django.forms import ModelForm
+from .models import Item
 
 
 PAYMENT_CHOICES = (
